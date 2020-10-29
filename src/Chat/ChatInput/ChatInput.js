bb72a64964c608ef771d8262b89cdb1d595d25fa
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-29 21:31:03 +0800

    finish homework

--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
+
+  setMessage = (event) => {
+    this.setState({
+      message: event.target.value,
+    });
+  };
+
+  sendMessage = () => {
+    this.props.updateMessage(this.state.message);
+    this.setState({
+      message: '',
+    });
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input type="text" onChange={this.setMessage} value={this.state.message} />
+        <button type="button" onClick={this.sendMessage}>
+          Send
+        </button>
       </footer>
     );
   }
